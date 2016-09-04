ba7ec6317790f992afeec4ac154640af92209286
DadiHall <dadih32@gmail.com>
2016-09-04 18:01:51 +0000

    Added cap files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+set :state, :production
+server '138.68.129.89', user: 'deploy', roles: %w{web app}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
